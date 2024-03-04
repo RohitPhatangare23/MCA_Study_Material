e63bf72acaf5460dd5f99040dceea906e99cb331
Rohit Phatangare <81705152+RohitPhatangare23@users.noreply.github.com>
2024-03-04 23:59:12 +0530

    Start 2nd sem lab, I started learning about dot net framework and create simple form

---
+++ 2nd_Semester/Advanced_Web_Technology/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name1 = textBox1.Text;
+            String email1 = textBox2.Text;
+            String mobile1 = textBox3.Text;
+            String password1 = textBox4.Text;
+
+            label12.Text = name1;
+            label13.Text = email1;
+            label14.Text = mobile1;
+            label15.Text = password1;
+        }
+    }
+}
